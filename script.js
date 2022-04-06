69ab3b21afa17b6ed71989259d1af6ebc5b44e4d
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-06 15:44:10 +0200

    Aggiunto output a video

--- script.js
+++ script.js
@@ -4,19 +4,20 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-const km = 100;
-const age = 17;
+const km = parseInt(prompt("Quanti km devi percorrere?"));
+const age = parseInt(prompt("Quanti anni hai?"));
 let price, finalPrice, discount = 0;
-price = km * 0.21
+price = km * 0.21;
 
 if(age < 18){
-  discount = (price / 100) * 20
-  finalPrice = price - discount
+  discount = (price / 100) * 20;
+  finalPrice = price - discount;
+  document.getElementById("price-output").innerHTML = `Complimenti, hai diritto ad uno sconto del 20% (equivalente a ${discount.toFixed(2)}€) che è stato applicato al prezzo intero del tuo biglietto (${price.toFixed(2)}€). <br> Il prezzo finale del tuo biglietto è quindi ${finalPrice.toFixed(2)}€`;
 } else if(age > 65){
-  discount = (price / 100) * 40
-  finalPrice = price - discount
+  discount = (price / 100) * 40;
+  finalPrice = price - discount;
+  document.getElementById("price-output").innerHTML = `Complimenti, hai diritto ad uno sconto del 40% (equivalente a ${discount.toFixed(2)}€) che è stato applicato al prezzo intero del tuo biglietto (${price.toFixed(2)}€). <br> Il prezzo finale del tuo biglietto è quindi ${finalPrice.toFixed(2)}€`;
 } else {
-  finalPrice = price
+  finalPrice = price;
+  document.getElementById("price-output").innerHTML = `Il prezzo finale del tuo biglietto è ${finalPrice.toFixed(2)}€`;
 }
-console.log(discount)
-console.log(finalPrice)
